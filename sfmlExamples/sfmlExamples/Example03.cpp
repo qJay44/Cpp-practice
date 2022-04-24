e08b011d08226b75c8a7027c28c343ce7977baf1
muivproject <m70155302@yandex.ru>
2022-04-24 04:14:27 +0300

    example 7

--- sfmlExamples/sfmlExamples/Example03.cpp
+++ sfmlExamples/sfmlExamples/Example03.cpp
@@ -1,48 +1,48 @@
-//#include <iostream>
-//#include "SFML/Graphics.hpp"
-//#include "SFML/Window.hpp"
-//#include "SFML/System.hpp"
-//
-//using namespace sf;
-//
-//int main()
-//{
-//	RenderWindow window(VideoMode(640, 400), "SFML works!", Style::Default);
-//	window.setFramerateLimit(90);
-//
-//	CircleShape circle(50.f);
-//	circle.setPosition(Vector2f(0.f, 0.f));
-//
-//	RectangleShape rect(Vector2f(50.f, 100.f));
-//	rect.setPosition(Vector2f(400.f, 200.f));
-//
-//	circle.setFillColor(Color(255, 115, 200, 200));
-//	rect.setFillColor(Color(55, 115, 200, 200));
-//
-//
-//	while (window.isOpen())
-//	{
-//		Event event;
-//		while (window.pollEvent(event))
-//		{
-//			if (event.type == Event::Closed)
-//				window.close();
-//		}
-//
-//		// Update
-//		circle.move(0.5f, 0.1f);
-//		circle.rotate(2.f);
-//		rect.move(-0.5f, -0.1f);
-//		rect.rotate(22.f);
-//
-//		window.clear();
-//
-//		// Draw everything
-//		window.draw(circle);
-//		window.draw(rect);
-//
-//		window.display();
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(640, 400), "SFML works!", Style::Default);
+	window.setFramerateLimit(90);
+
+	CircleShape circle(50.f);
+	circle.setPosition(Vector2f(0.f, 0.f));
+
+	RectangleShape rect(Vector2f(50.f, 100.f));
+	rect.setPosition(Vector2f(400.f, 200.f));
+
+	circle.setFillColor(Color(255, 115, 200, 200));
+	rect.setFillColor(Color(55, 115, 200, 200));
+
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		// Update
+		circle.move(0.5f, 0.1f);
+		circle.rotate(2.f);
+		rect.move(-0.5f, -0.1f);
+		rect.rotate(22.f);
+
+		window.clear();
+
+		// Draw everything
+		window.draw(circle);
+		window.draw(rect);
+
+		window.display();
+	}
+
+	return 0;
+}
--- sfmlExamples/sfmlExamples/Example07.cpp
+++ sfmlExamples/sfmlExamples/Example07.cpp
@@ -10,6 +10,21 @@ int main()
 	RenderWindow window(VideoMode(640, 480), "Simple game");
 	window.setFramerateLimit(90);
 
+	CircleShape hoop;
+	int dir = 0;
+	hoop.setRadius(50.f);
+	hoop.setFillColor(Color::White);
+	hoop.setOutlineThickness(2.f);
+	hoop.setOutlineColor(Color::Blue);
+	hoop.setPosition(Vector2f(0.f, 10.f));
+
+	CircleShape ball;
+	bool isShot = false;
+	ball.setRadius(20.f);
+	ball.setFillColor(Color::Red);
+	ball.setPosition(Vector2f(0.f, window.getSize().y - ball.getRadius() * 3));
+
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -17,8 +32,48 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
+			if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+				window.close();
 		}
+
+		// Update hoop
+
+		if (hoop.getPosition().x <= 0)
+			dir = 1;
+		else if (hoop.getPosition().x + hoop.getRadius() * 2 >= window.getSize().x)
+			dir = 0;
+
+		if (dir == 0)
+		{
+			hoop.move(-5.f, 0);
+		}
+		else
+		{
+			hoop.move(5.f, 0);
+		}
+		
+		// Update ball
+
+		if (Mouse::isButtonPressed(Mouse::Left))
+			isShot = true;
+
+		if (!isShot)
+			ball.setPosition(Mouse::getPosition(window).x, ball.getPosition().y);
+		else
+			ball.move(0.f, -5.f);
+
+		// Collision ball
+
+		if (ball.getPosition().y < 0 || ball.getGlobalBounds().intersects(hoop.getGlobalBounds()))
+		{
+			isShot = false;
+			ball.setPosition(ball.getPosition().x, window.getSize().y - ball.getRadius() * 3);
+		}
+
 		window.clear();
+
+		window.draw(hoop);
+		window.draw(ball);
 
 		window.display();
 	}
