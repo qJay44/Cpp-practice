d82aec3811ffb9d3f9802e9b1f33a8b66e181e31
qJay44 <m70155302@yandex.ru>
2022-04-29 21:38:31 +0300

    rename

---
+++ Cpp Tutorial/Cpp Tutorial/Dice.cpp
@@ -0,0 +1,26 @@
+#include "Dice.h"
+
+int Dice::ID = 0;
+
+// Static functions
+int Dice::getStaticID()
+{
+	return Dice::ID;
+}
+
+Dice::Dice(const int value, const std::string color)
+	: value(value)
+{
+	this->color = color;
+	Dice::ID++;
+}
+
+Dice::~Dice()
+{
+}
+
+// Functions
+const int Dice::toss() const
+{
+	return rand() % this->value + 1;
+}
