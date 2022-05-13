4fbbd22fb869ebd504b2891b20dd9c14aaf87399
qJay44 <m70155302@yandex.ru>
2022-05-14 02:23:15 +0300

    12 p4

--- SFML/sfmlExamples/Example12.cpp
+++ SFML/sfmlExamples/Example12.cpp
@@ -44,6 +44,7 @@ public:
 	~Player() {}
 
 	inline Sprite& getShape() { return this->shape; }
+	inline const int getHP(const int mode) { return mode == 0 ? this->HP : this->HP_Max; } const
 
 	inline const float getX() const { return this->shape.getPosition().x; }
 	inline const float getY() const { return this->shape.getPosition().y; }
@@ -76,7 +77,13 @@ public:
 	}
 
 	inline Sprite& getShape() { return this->shape; }
+	inline const int getHP(const int mode) { return mode == 0 ? this->HP : this->HP_Max; } const
+
+	inline const float getX() const { return this->shape.getPosition().x; }
+	inline const float getY() const { return this->shape.getPosition().y; }
+
 	void moveEnemy(const float x, const float y) { this->shape.move(x, y); }
+	void hitEnemy() { this->HP--; }
 
 	~Enemy() {};
 
@@ -112,10 +119,29 @@ int main()
 	Texture bulletTex;
 	bulletTex.loadFromFile("Textures/missileTex01.png");
 
+	// UI init
+	Text scoreText;
+	scoreText.setFont(font);
+	scoreText.setCharacterSize(20);
+	scoreText.setFillColor(Color::White);
+	scoreText.setPosition(10.f, 10.f);
+
+	Text gameOverText;
+	gameOverText.setFont(font);
+	gameOverText.setCharacterSize(30);
+	gameOverText.setFillColor(Color::Red);
+	gameOverText.setPosition(100.f, h / 2.f);
+	gameOverText.setString("GAME OVER!");
+
 	// Player init
+	int score = 0;
 	Player player(&playerTex);
 	int shootTimer = 20;
 	const float step = 10.f;
+	Text hpText;
+	hpText.setFont(font);
+	hpText.setCharacterSize(12);
+	hpText.setFillColor(Color::White);
 
 	// Bullets
 	std::vector<Bullet> bullets;
@@ -123,6 +149,10 @@ int main()
 	// Enemy init
 	int enemySpawnTimer = 0;
 	std::vector<Enemy> enemies;
+	Text eHpText;
+	eHpText.setFont(font);
+	eHpText.setCharacterSize(12);
+	eHpText.setFillColor(Color::White);
 
 	while (window.isOpen())
 	{
@@ -133,88 +163,104 @@ int main()
 				window.close();
 		}
 
-		//// Update
+		hpText.setPosition(player.getX(), player.getY() - hpText.getGlobalBounds().height);
+		hpText.setString(std::to_string(player.getHP(0)) + "/" + std::to_string(player.getHP(1)));
 
-		// Player
-		if (Keyboard::isKeyPressed(Keyboard::W)) player.movePlayer(0.f  , -step);
-		if (Keyboard::isKeyPressed(Keyboard::A)) player.movePlayer(-step, 0.f  );
-		if (Keyboard::isKeyPressed(Keyboard::S)) player.movePlayer(0.f  , step);
-		if (Keyboard::isKeyPressed(Keyboard::D)) player.movePlayer(step , 0.f  );
-
-		// Collision with window
-
-		if (player.getX() <= 0) player.movePlayer(step, 0.f);
-		if (player.getY() <= 0) player.movePlayer(0.f, step);
-		if (player.getX() + player.getWidth() >= w) player.movePlayer(-step, 0.f);
-		if (player.getY() + player.getHeight() >= h) player.movePlayer(0.f, -step);
-
-		// Update controls
-
-		if (shootTimer < 20) shootTimer++;
-
-		if (Mouse::isButtonPressed(Mouse::Left) && shootTimer >= 20)
+		if (player.getHP(0) > 0)
 		{
-			bullets.push_back(Bullet(&bulletTex, player.getShape().getPosition()));
-			shootTimer = 0;
-		}
+			//// Update
 
-		// Bullets
+			// Player
 
-		for (size_t i = 0; i < bullets.size(); i++)
-		{
-			// Move
-			bullets[i].moveBullet(20.f, 0.f);
+			if (Keyboard::isKeyPressed(Keyboard::W)) player.movePlayer(0.f, -step);
+			if (Keyboard::isKeyPressed(Keyboard::A)) player.movePlayer(-step, 0.f);
+			if (Keyboard::isKeyPressed(Keyboard::S)) player.movePlayer(0.f, step);
+			if (Keyboard::isKeyPressed(Keyboard::D)) player.movePlayer(step, 0.f);
 
-			// Out of the window bounds
-			if (bullets[i].getShape().getPosition().x > w)
+
+			// Collision with window
+
+			if (player.getX() <= 0) player.movePlayer(step, 0.f);
+			if (player.getY() <= 0) player.movePlayer(0.f, step);
+			if (player.getX() + player.getWidth() >= w) player.movePlayer(-step, 0.f);
+			if (player.getY() + player.getHeight() >= h) player.movePlayer(0.f, -step);
+
+			// Update controls
+
+			if (shootTimer < 5) shootTimer++;
+
+			else if (Mouse::isButtonPressed(Mouse::Left))
 			{
-				bullets.erase(bullets.begin() + i);
-				break;
+				bullets.push_back(Bullet(&bulletTex, player.getShape().getPosition()));
+				shootTimer = 0;
 			}
 
-			// Enemy collision
-			for (size_t k = 0; k < enemies.size(); k++)
+			// Bullets
+
+			for (size_t i = 0; i < bullets.size(); i++)
 			{
-				if (bullets[i].getShape().getGlobalBounds().intersects(enemies[k].getShape().getGlobalBounds()))
+				// Move
+				bullets[i].moveBullet(20.f, 0.f);
+
+				// Out of the window bounds
+				if (bullets[i].getShape().getPosition().x > w)
 				{
-					enemies.erase(enemies.begin() + k);
 					bullets.erase(bullets.begin() + i);
+					break;
+				}
+
+				// Enemy collision
+				for (size_t k = 0; k < enemies.size(); k++)
+				{
+					if (bullets[i].getShape().getGlobalBounds().intersects(enemies[k].getShape().getGlobalBounds()))
+					{
+						if (enemies[k].getHP(0) <= 1)
+						{
+							score += enemies[k].getHP(1);
+							enemies.erase(enemies.begin() + k);
+						}
+						else enemies[k].hitEnemy();
+
+						bullets.erase(bullets.begin() + i);
+
+						break;
+					}
+				}
+			}
+
+			// Enemy
+
+			if (enemySpawnTimer < 25) enemySpawnTimer++;
+			else
+			{
+				enemies.push_back(Enemy(&enemyTex, w, h));
+				enemySpawnTimer = 0;
+			}
+
+			for (size_t i = 0; i < enemies.size(); i++)
+			{
+				enemies[i].moveEnemy(-2.f, 0.f);
+
+				Sprite enemy = enemies[i].getShape();
+
+				if (enemy.getPosition().x + enemy.getGlobalBounds().width <= 0)
+				{
+					enemies.erase(enemies.begin() + i);
+					break;
+				}
+
+				if (enemy.getGlobalBounds().intersects(player.getShape().getGlobalBounds()))
+				{
+					enemies.erase(enemies.begin() + i);
+					player.hitPlayer();
 
 					break;
 				}
 			}
 		}
 
-		// Enemy
-
-		if (enemySpawnTimer < 25) enemySpawnTimer++;
-		else
-		{
-			enemies.push_back(Enemy(&enemyTex, w, h));
-			enemySpawnTimer = 0;
-		}
-
-		for (size_t i = 0; i < enemies.size(); i++)
-		{
-			enemies[i].moveEnemy(-2.f, 0.f);
-
-			Sprite enemy = enemies[i].getShape();
-
-			if (enemy.getPosition().x + enemy.getGlobalBounds().width <= 0)
-			{
-				enemies.erase(enemies.begin() + i);
-				break;
-			}
-
-			if (enemy.getGlobalBounds().intersects(player.getShape().getGlobalBounds()))
-			{
-				enemies.erase(enemies.begin() + i);
-				player.hitPlayer();
-
-				break;
-			}
-		}
-
+		// UI update
+		scoreText.setString("Score: " + std::to_string(score));
 
 		//// Draw
 
@@ -232,8 +278,18 @@ int main()
 		// Enemy
 		for (size_t i = 0; i < enemies.size(); i++)
 		{
+			eHpText.setString(std::to_string(enemies[i].getHP(0)) + "/" + std::to_string(enemies[i].getHP(1)));
+			eHpText.setPosition(enemies[i].getX(), enemies[i].getY() - eHpText.getGlobalBounds().height);
+			window.draw(eHpText);
 			window.draw(enemies[i].getShape());
 		}
+
+		// UI
+
+		window.draw(scoreText);
+		window.draw(hpText);
+
+		if (player.getHP(0) == 0) window.draw(gameOverText);
 
 		window.display();
 	}
